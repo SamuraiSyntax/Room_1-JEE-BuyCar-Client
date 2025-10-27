0f2cef409e5f8651e084606846ce02bbfe2f4d7f
seynaboumbayeinfo <seynabou.mbaye.info@gmail.com>
2025-10-27 12:33:00 +0100

    utilisateurDao et model

---
+++ src/main/java/org/eclipse/models/Utilisateur.java
@@ -0,0 +1,102 @@
+package org.eclipse.models;
+
+public class Utilisateur {
+	private int id;
+	private String nom;
+	private String prenom;
+	private String compte;
+	private String email;
+	private String password;
+	private int role;
+	private String telephone;
+	
+	
+	
+	public Utilisateur(int id, String nom, String prenom, String email, String password) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.password = password;
+	}
+	public Utilisateur(int id, String nom, String prenom, String compte, String email, String password, int role,
+			String telephone) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.compte = compte;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+		this.telephone = telephone;
+	}
+	public Utilisateur(String nom, String prenom, String compte, String email, String password, int role,
+			String telephone) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.compte = compte;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+		this.telephone = telephone;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getCompte() {
+		return compte;
+	}
+	public void setCompte(String compte) {
+		this.compte = compte;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getRole() {
+		return role;
+	}
+	public void setRole(int role) {
+		this.role = role;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	@Override
+	public String toString() {
+		return "Utilisateur [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", compte=" + compte + ", email="
+				+ email + ", password=" + password + ", role=" + role + ", telephone=" + telephone + "]";
+	}
+	
+	
+
+}
---
+++ src/main/java/org/eclipse/repositories/UtilisateurDao.java
@@ -0,0 +1,134 @@
+package org.eclipse.repositories;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.config.MySqlConnection;
+import org.eclipse.models.Utilisateur;
+
+public class UtilisateurDao implements GenericDao<Utilisateur, Integer> {
+
+	@Override
+	public List<Utilisateur> findAll() {
+		List<Utilisateur> utilisateurs = null;
+		Connection connection = MySqlConnection.getConnection();
+		if (connection != null) {
+			try {
+				Statement statement = connection.createStatement();
+				String select = "SELECT * FROM Utilisateur";
+				ResultSet resultSet = statement.executeQuery(select);
+				utilisateurs = new ArrayList<Utilisateur>();
+				while (resultSet.next()) {
+					int id = resultSet.getInt("id_user");
+					String nom = resultSet.getString("nom");
+					String prenom = resultSet.getString("prenom");
+					String email = resultSet.getString("email");
+					String password = resultSet.getString("mot_de_passe");
+					String compte = resultSet.getString("nom_compte");
+					int role = resultSet.getInt("id_role");
+					String telephone = resultSet.getString("telephone");
+					utilisateurs.add(new Utilisateur(id, nom, prenom, compte, email, password, role, telephone));
+				}
+			} catch (Exception e) {
+				System.err.println("Problème de récupération d'utilisateurs");
+			}
+		}
+		return utilisateurs;
+	}
+
+	@Override
+	public Utilisateur findById(Integer id) {
+		Connection connection = MySqlConnection.getConnection();
+		if (connection != null) {
+			try {
+				String select = "SELECT * FROM utilisateur WHERE id = ?";
+				PreparedStatement statement = connection.prepareStatement(select);
+				statement.setInt(1, id);
+				ResultSet resultSet = statement.executeQuery();
+				if (resultSet.next()) {
+					String nom = resultSet.getString("nom");
+					String prenom = resultSet.getString("prenom");
+					String email = resultSet.getString("email");
+					String password = resultSet.getString("mot_de_passe");
+					String compte = resultSet.getString("nom_compte");
+					int role = resultSet.getInt("id_role");
+					String telephone = resultSet.getString("telephone");
+					return (new Utilisateur(id, nom, prenom, compte, email, password, role, telephone));
+				}
+			} catch (Exception e) {
+				System.err.println("Problème de récupération d'utilisateurs");
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public Utilisateur save(Utilisateur model) {
+		Connection connection = MySqlConnection.getConnection();
+		if (connection != null) {
+			try {
+				String insert = "INSERT INTO utilisateur (nom, prenom, email, mot_de_passe) VALUES (?, ?, ?, ?)";
+
+				PreparedStatement statement = connection.prepareStatement(insert,
+						PreparedStatement.RETURN_GENERATED_KEYS);
+				statement.setString(1, model.getNom());
+				statement.setString(2, model.getPrenom());
+				statement.setString(3, model.getEmail());
+				statement.setString(4, model.getPassword());
+
+				statement.executeUpdate();
+				ResultSet result = statement.getGeneratedKeys();
+				if (result.next()) {
+					model.setId(result.getInt(1));
+					return model;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.err.println("Problème d'insertion de utilisateur");
+			}
+		}
+		return null;
+
+	}
+
+	@Override
+	public Utilisateur update(Utilisateur model) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(Integer id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Utilisateur findByEmailAndPassword(String email, String motDePasse) {
+		Connection connection = MySqlConnection.getConnection();
+		if (connection != null) {
+			try {
+				String sql = "SELECT * FROM utilisateur WHERE email = ? AND motDePasse = ?";
+				PreparedStatement statement = connection.prepareStatement(sql);
+				statement.setString(1, email);
+				statement.setString(2, motDePasse);
+				ResultSet resultSet = statement.executeQuery();
+
+				if (resultSet.next()) {
+					int id = resultSet.getInt("id");
+					String nom = resultSet.getString("nom");
+					String prenom = resultSet.getString("prenom");
+					String motDePasseDb = resultSet.getString("motDePasse");
+
+					return new Utilisateur(id, nom, prenom, email, motDePasseDb);
+				}
+			} catch (Exception e) {
+				System.err.println("Erreur lors de la recherche de l'utilisateur : " + e.getMessage());
+			}
+		}
+		return null;
+	}
+}
